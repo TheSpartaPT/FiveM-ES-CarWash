37166a0db878b9e7a9015a4b6354a3f6d0db1b78
Pedro Silva <sparta14gaming@gmail.com>
2017-08-28 21:44:24 +0100

    Update es_carwash_server.lua
    Renamed all client and server side events

--- es_carwash/es_carwash_server.lua
+++ es_carwash/es_carwash_server.lua
@@ -6,20 +6,20 @@ price = 100 -- you may edit this to your liking. if "enableprice = false" ignore
 
 --DO-NOT-EDIT-BELLOW-THIS-LINE--
 
-RegisterServerEvent('carwash:checkmoney')
-AddEventHandler('carwash:checkmoney', function ()
+RegisterServerEvent('es_carwash:checkmoney')
+AddEventHandler('es_carwash:checkmoney', function ()
 	TriggerEvent('es:getPlayerFromId', source, function (user)
 		if enableprice == true then
 			userMoney = user.getMoney()
 			if userMoney >= price then
 				user.removeMoney(price)
-				TriggerClientEvent('carwash:success', source, price)
+				TriggerClientEvent('es_carwash:success', source, price)
 			else
 				moneyleft = price - userMoney
-				TriggerClientEvent('carwash:notenoughmoney', source, moneyleft)
+				TriggerClientEvent('es_carwash:notenoughmoney', source, moneyleft)
 			end
 		else
-			TriggerClientEvent('carwash:free', source)
+			TriggerClientEvent('es_carwash:free', source)
 		end
 	end)
 end)
